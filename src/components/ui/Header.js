7e5de7197d14bfdb9104ba99a9222a5b96bd438b
paul8768 <tripleji8768@gmail.com>
2022-07-21 23:33:49 +0900

    리소스 추가 및 헤더추가

---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import { AppBar, Toolbar } from "@material-ui/core";
+import React from "react";
+
+const Header = () => {
+  return (
+    <AppBar position="fixed">
+      <Toolbar>Paul Development</Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
