63961f09317dfea2b1da234307ebae6e4a68440a
paul8768 <tripleji8768@gmail.com>
2022-08-07 19:51:39 +0900

    Header Tab 추가

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,11 +1,67 @@
-import { AppBar, Toolbar } from "@material-ui/core";
 import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  useScrollTrigger,
+  makeStyles,
+  Tabs,
+  Tab,
+} from "@material-ui/core";
+
+import companyLogo from "../..//assets/logo.svg";
+
+function ElevationScroll(props) {
+  const { children } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: "3em",
+  },
+  logo: {
+    height: "7rem",
+  },
+  tabContainer: {
+    marginLeft: "auto",
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: "25px",
+  },
+}));
 
 const Header = () => {
+  const { toolbarMargin, logo, tabContainer, tab } = useStyles();
+
   return (
-    <AppBar position="fixed">
-      <Toolbar>Paul Development</Toolbar>
-    </AppBar>
+    <>
+      <ElevationScroll>
+        <AppBar position="fixed">
+          <Toolbar disableGutters>
+            <img alt="company logo" className={logo} src={companyLogo} />
+            <Tabs className={tabContainer}>
+              <Tab className={tab} label="HOME" />
+              <Tab className={tab} label="SERVICES" />
+              <Tab className={tab} label="THE REVOLUTION" />
+              <Tab className={tab} label="ABOUT US" />
+              <Tab className={tab} label="CONTACT US" />
+            </Tabs>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={toolbarMargin} />
+    </>
   );
 };
 
---
+++ src/components/ui/Theme.js
@@ -0,0 +1,27 @@
+import { createMuiTheme } from "@material-ui/core";
+
+const arcBlue = "#0872b9";
+const arcOrange = "#ffba60";
+
+export default createMuiTheme({
+  palette: {
+    common: {
+      arcBlue: `${arcBlue}`,
+      arcOrange: `${arcOrange}`,
+    },
+    primary: {
+      main: `${arcBlue}`,
+    },
+    secondary: {
+      main: `${arcOrange}`,
+    },
+  },
+  typography: {
+    tab: {
+      fontFamily: "Raleway",
+      textTransform: "none",
+      fontWeight: 700,
+      fontSize: "1em",
+    },
+  },
+});
